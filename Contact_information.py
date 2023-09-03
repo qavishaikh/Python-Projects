1115eb5e4fb9b4a8c6a6350d2eacdf4735f168af
Qavi <qavi.shaikh11@gmail.com>
2023-09-03 16:42:24 +0500

    Modify Contact Information Project

--- Contact_information.py
+++ Contact_information.py
@@ -2,27 +2,33 @@
 contacts = {}
 
 while True:
-    print("Menu:")
+    print("\n ----- MENU ----- \n")
     print("1. Add a contact")
     print("2. Update a contact")
     print("3. Delete a contact")
     print("4. Search for a contact")
     print("5. List all contacts")
-    print("6. Exit")
+    print("6. Exit \n")
     
-    choice = input("What you want to do ? \n Enter Your Choice: ")
+    choice = input("What you want to do?\nEnter Your Choice: ")
     
     if choice == "1":
         name = input("Enter the name of the contact: ")
         phone = input("Enter the phone number: ")
-        contacts[name] = phone
-        print(f"{name} has been added to your contacts.")
+        if len(phone) != 11:
+            print("Invalid phone number. Please enter exactly 11 digits.")
+        else:
+            contacts[name] = phone
+            print(f"{name} has been added to your contacts.")
     elif choice == "2":
         name = input("Enter the name of the contact to update: ")
         if name in contacts:
             phone = input("Enter the new phone number: ")
-            contacts[name] = phone
-            print(f"{name}'s phone number has been updated.")
+            if len(phone) != 11:
+                print("Invalid phone number. Please enter exactly 11 digits.")
+            else:
+                contacts[name] = phone
+                print(f"{name}'s phone number has been updated.")
         else:
             print(f"{name} is not in your contacts.")
     elif choice == "3":
@@ -40,10 +46,13 @@ while True:
         else:
             print(f"{name} is not in your contacts.")
     elif choice == "5":
-        for name, phone in contacts.items():
-            print(f"{name}: {phone}")
+        if not contacts:
+            print("Your contacts list is empty.")
+        else:
+            for name, phone in contacts.items():
+                print(f"{name}: {phone}")
     elif choice == "6":
-        print("Goodbye! \n Created By Qavi Shaikh")
+        print("Goodbye!\nCreated By Qavi Shaikh")
         break
     else:
-        print("Invalid choice. Please enter a valid option (1-7).")
+        print("Invalid choice. Please enter a valid option (1-6).")
